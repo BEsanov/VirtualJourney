fd4f0213c31d5d18d789261fb1d5ee72bf52e984
Bakhtiyor Esanov <Bakhtiyor.Esanov@lut.fi>
2020-08-10 03:06:55 +0300

    Add project files.

---
+++ VirtualJourney/Views/HomePage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VirtualJourney.Views
+{
+
+    public partial class HomePage : ContentPage
+    {
+
+
+        public HomePage()
+        {
+            InitializeComponent();
+
+        }
+               
+    }
+}
---
+++ VirtualJourney/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using VirtualJourney.ViewModels;
+
+namespace VirtualJourney.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
